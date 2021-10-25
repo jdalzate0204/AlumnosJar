f246599db81fcfcaacbf3b2d37b043682fcd05ca
jdalzate0204 <james.alzate22@hotmail.com>
2021-10-25 16:43:41 -0500

    IAlumnoService

--- src/main/java/co/edu/unicundi/alumnosjar/service/IAlumnoService.java
+++ src/main/java/co/edu/unicundi/alumnosjar/service/IAlumnoService.java
@@ -1,5 +1,7 @@
 package co.edu.unicundi.alumnosjar.service;
 
+import co.edu.unicundi.alumnosjar.entity.Alumno;
+import java.util.List;
 import javax.ejb.Local;
 
 /**
@@ -8,5 +10,9 @@ import javax.ejb.Local;
  */
 @Local
 public interface IAlumnoService {
-    
+    public void guardar(Alumno obj);
+    public List<Alumno> listar();
+    public Alumno listarPorId(Integer id);
+    public void editar(Alumno obj);
+    public void eliminar(Integer id);
 }
--- src/main/java/co/edu/unicundi/alumnosjar/service/impl/AlumnoServiceImpl.java
+++ src/main/java/co/edu/unicundi/alumnosjar/service/impl/AlumnoServiceImpl.java
@@ -1,7 +1,9 @@
 package co.edu.unicundi.alumnosjar.service.impl;
 
+import co.edu.unicundi.alumnosjar.entity.Alumno;
 import co.edu.unicundi.alumnosjar.repository.IAlumnoRepo;
 import co.edu.unicundi.alumnosjar.service.IAlumnoService;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
@@ -11,7 +13,32 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class AlumnoServiceImpl implements IAlumnoService{
-    
-    @EJB
-    public IAlumnoRepo repo;
+
+    /*@EJB
+    public IAlumnoRepo repo;*/
+
+    @Override
+    public void guardar(Alumno obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Alumno> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Alumno listarPorId(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void editar(Alumno obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
